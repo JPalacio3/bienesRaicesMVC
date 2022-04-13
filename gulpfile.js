a9cfd52911a5ab2c1ffda8ed09031c5eacf89e0b
J Palacio <joelpalacio630@gmail.com>
2022-04-13 11:53:26 -0500

    ejecutando node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,89 @@
-import { src, dest, watch, parallel } from 'gulp';
-import sass from 'gulp-sass';
-import autoprefixer from 'autoprefixer';
-import postcss from 'gulp-postcss';
-import { init, write } from 'gulp-sourcemaps';
-import cssnano from 'cssnano';
-import concat from 'gulp-concat';
-import terser from 'gulp-terser-js';
-import rename from 'gulp-rename';
-import imagemin from 'gulp-imagemin';
-import notify from 'gulp-notify';
-import cache from 'gulp-cache';
-import webp from 'gulp-webp';
+const { src, dest, watch, parallel } = require('gulp');
 
-const paths = {
-    scss: 'src/scss/**/*.scss',
-    js: 'src/js/**/*.js',
-    imagenes: 'src/img/**/*'
+// CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+// Javascript
+const terser = require('gulp-terser-js');
+
+function css( done ) {
+    src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
+        .pipe(sourcemaps.init())
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
+        .pipe( dest('build/css') ) // Almacenarla en el disco duro
+    done();
 }
 
-// css es una función que se puede llamar automaticamente
-function css() {
-    return src(paths.scss)
-        .pipe(init())
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        // .pipe(postcss([autoprefixer()]))
-        .pipe(write('.'))
-        .pipe(dest('./public/build/css'));
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
 }
 
-
-function javascript() {
-    return src(paths.js)
-        .pipe(init())
-        .pipe(concat('bundle.js')) // final output file name
-        .pipe(terser())
-        .pipe(write('.'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(dest('./public/build/js'))
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
 }
 
-function imagenes() {
-    return src(paths.imagenes)
-        .pipe(cache(imagemin({ optimizationLevel: 3 })))
-        .pipe(dest('./public/build/img'))
-        .pipe(notify({ message: 'Imagen Completada' }));
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
 }
 
-function versionWebp() {
-    return src(paths.imagenes)
-        .pipe(webp())
-        .pipe(dest('./public/build/img'))
-        .pipe(notify({ message: 'Imagen Completada' }));
+function javascript( done ) {
+    src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
 }
 
-
-function watchArchivos() {
-    watch(paths.scss, css);
-    watch(paths.js, javascript);
-    watch(paths.imagenes, imagenes);
-    watch(paths.imagenes, versionWebp);
+function dev( done ) {
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
+    done();
 }
 
-const _default = parallel(css, javascript, imagenes, versionWebp, watchArchivos);
-export { _default as default };
+function tarea (done) {
+    console.log('Desde la primera tarea');
+    done();
+}
+ 
+exports.tarea = tarea;
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev) ;
